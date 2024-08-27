16cfde6fb81709ee9b59398f2949001c2959ad35
mt01581 <mt01581@surrey.ac.uk>
2024-08-27 14:32:40 +0100

    navBar done

---
+++ components/ui/navBar.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import { usePathname } from "next/navigation";
+import React from "react";
+
+export default function NavBar() {
+
+    const pathname = usePathname();
+
+    const navItems = [
+        {
+          label: "Home",
+          link: "/",
+          icon: (
+            <svg
+              className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+            </svg>
+          ),
+        },
+        {
+          label: "Matches",
+          link: "/matches",
+          icon: (
+            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  className="icon icon-tabler icons-tabler-filled icon-tabler-briefcase w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M22 13.478v4.522a3 3 0 0 1 -3 3h-14a3 3 0 0 1 -3 -3v-4.522l.553 .277a20.999 20.999 0 0 0 18.897 -.002l.55 -.275zm-8 -11.478a3 3 0 0 1 3 3v1h2a3 3 0 0 1 3 3v2.242l-1.447 .724a19.002 19.002 0 0 1 -16.726 .186l-.647 -.32l-1.18 -.59v-2.242a3 3 0 0 1 3 -3h2v-1a3 3 0 0 1 3 -3h4zm-2 8a1 1 0 0 0 -1 1a1 1 0 1 0 2 .01c0 -.562 -.448 -1.01 -1 -1.01zm2 -6h-4a1 1 0 0 0 -1 1v1h6v-1a1 1 0 0 0 -1 -1z" /></svg> )
+        },
+        {
+          label: "Companies",
+          link: "/companies",
+          icon: (
+            <svg 
+            className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500 icon icon-tabler icons-tabler-outline icon-tabler-buildings"
+    
+             xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 21v-15c0 -1 1 -2 2 -2h5c1 0 2 1 2 2v15" /><path d="M16 8h2c1 0 2 1 2 2v11" /><path d="M3 21h18" /><path d="M10 12v0" /><path d="M10 16v0" /><path d="M10 8v0" /><path d="M7 12v0" /><path d="M7 16v0" /><path d="M7 8v0" /><path d="M17 12v0" /><path d="M17 16v0" /></svg>
+          )
+        },
+        {
+          label: "Tips",
+          link: "/tips",
+          icon: (
+            <svg
+            className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500 icon icon-tabler icons-tabler-filled icon-tabler-bulb"
+            
+            xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 11a1 1 0 0 1 .117 1.993l-.117 .007h-1a1 1 0 0 1 -.117 -1.993l.117 -.007h1z" /><path d="M12 2a1 1 0 0 1 .993 .883l.007 .117v1a1 1 0 0 1 -1.993 .117l-.007 -.117v-1a1 1 0 0 1 1 -1z" /><path d="M21 11a1 1 0 0 1 .117 1.993l-.117 .007h-1a1 1 0 0 1 -.117 -1.993l.117 -.007h1z" /><path d="M4.893 4.893a1 1 0 0 1 1.32 -.083l.094 .083l.7 .7a1 1 0 0 1 -1.32 1.497l-.094 -.083l-.7 -.7a1 1 0 0 1 0 -1.414z" /><path d="M17.693 4.893a1 1 0 0 1 1.497 1.32l-.083 .094l-.7 .7a1 1 0 0 1 -1.497 -1.32l.083 -.094l.7 -.7z" /><path d="M14 18a1 1 0 0 1 1 1a3 3 0 0 1 -6 0a1 1 0 0 1 .883 -.993l.117 -.007h4z" /><path d="M12 6a6 6 0 0 1 3.6 10.8a1 1 0 0 1 -.471 .192l-.129 .008h-6a1 1 0 0 1 -.6 -.2a6 6 0 0 1 3.6 -10.8z" /></svg>
+          )
+        }
+      ];
+    
+
+    return (
+        <div className="fixed bottom-0 z-50 w-full flex justify-center mb-5">
+        <div id="navBar" className="w-5/6 sm:w-4/6 md:w-3/6 xl:w-2/6 bg-white dark:bg-gray-700 rounded-[2rem] p-2">
+          <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
+            {
+              navItems.map((item) => {
+                const isActive = pathname === item.link;
+                const iconColor = isActive ? "rgba(242, 143, 38, 1)" : "currentColor";
+                
+                return (
+                
+                    <a
+                      key={item.label}
+                      href={item.link}
+                      className="flex flex-col items-center justify-center text-white py-2"
+                    >
+                        {React.cloneElement(item.icon, { fill: iconColor, stroke: iconColor })}
+                      <span className="text-xs">{item.label}</span>
+                    </a>
+                  );
+              })
+            }
+          </div>
+        </div>
+      </div>
+    )
+}
